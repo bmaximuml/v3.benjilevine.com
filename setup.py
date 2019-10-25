9c53c26d096b92eefe1165c77a6d1fb3e1645064
Benji Levine <benji@benjilevine.com>
2019-10-25 17:53:19 +0200

    Add setuptools files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='benjilevine.com',
+    version='0.0.1.dev',
+    long_description=__doc__,
+    packages=['benjilevine.com'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask'
+    ]
+)
