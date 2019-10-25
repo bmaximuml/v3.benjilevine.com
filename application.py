88195d8e8d9990f13bbfa1bdae980cde8486b8e6
Benji Levine <benji@benjilevine.com>
2019-10-25 23:39:44 +0200

    Elastic Beanstalk compatibility (#11)
    * Change app variable to application
    This is the variable that elastic beanstalk looks for
    
    * Allow the application to be run from the application.py file
    
    * Add requirements.txt file
    Only list direct dependencies
    
    * Move site resources into top directory
    
    * Use setuptools' find_packages function
    
    * Add author info to setup.py
    
    * Add py files to source distributions
    
    * Add elastic beanstalk config file
    
    * Set zip_safe deployment to True
    
    * Remove .dev from explicit version number
    This is added automatically by setuptools
    Leaving it in results in a duplicated .dev suffix
    
    * Include requirements.txt to manifest

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from datetime import datetime
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def about():
     return render_template('index.html', year=datetime.now().year)
 
+if __name__ == '__main__':
+    application.debug = True
+    application.run()
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='benjilevine.com',
-    version='0.1.dev',
+    version='0.1',
     long_description=__doc__,
-    packages=['benjilevine.com'],
+    packages=find_packages(),
     include_package_data=True,
-    zip_safe=False,
+    zip_safe=True,
     install_requires=[
         'datetime',
         'Flask'
-    ]
+    ],
+    author='Benji Levine',
+    author_email='benji@benjilevine.com',
+    url='https://github.com/benjilev08/v3.benjilevine.com'
 )
