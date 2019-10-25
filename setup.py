2712db1d13eae12011b06ba12e89fd425c16214a
Benji Levine <benji@benjilevine.com>
2019-10-25 22:10:56 +0200

    Use setuptools' find_packages function

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='benjilevine.com',
     version='0.1.dev',
     long_description=__doc__,
-    packages=['benjilevine.com'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
