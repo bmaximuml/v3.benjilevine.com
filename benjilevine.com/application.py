95dcb037e23c6d79d60b7f7d3267cea20b14b8ba
Benji Levine <benji@benjilevine.com>
2019-10-25 20:17:41 +0200

    Structure (#8)
    * Correct text in footer
    
    * Set general structure for main page
    
    * Show title in header bar

--- benjilevine.com/application.py
+++ benjilevine.com/application.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def about():
-    return render_template('index.html')
+    return render_template('index.html', year=datetime.now().year)
 
