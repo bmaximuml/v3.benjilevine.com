18360bee3d2b7c83a7f2ff279ff38c9a2202bf4c
Benji Levine <benji@benjilevine.com>
2019-10-26 17:25:26 +0200

    Add appropriate spacing

--- application.py
+++ application.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from datetime import datetime
 application = Flask(__name__)
 
+
 @application.route('/')
 def about():
     return render_template('index.html', year=datetime.now().year)
+
 
 if __name__ == '__main__':
     application.debug = True
