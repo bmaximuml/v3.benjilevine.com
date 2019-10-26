1b6ea4f1742ae12364b5e64f44e299622f2ed392
Benji Levine <benji@benjilevine.com>
2019-10-26 17:39:56 +0200

    Create ContactForm class using flask-wtform

--- application.py
+++ application.py
@@ -1,12 +1,32 @@
 from datetime import datetime
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
 from json import loads
 from os import environ
+from wtforms.fields import StringField, SubmitField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, length
+
+
 application = Flask(__name__)
 application.secret_key = environ['FLASK_SECRET_KEY']
 
 
 @application.route('/')
+class ContactForm(FlaskForm):
+    name = StringField('Name',
+                       validators=[DataRequired(), length(max=200)],
+                       render_kw={"placeholder": "Name", "class": "input"})
+    email = EmailField('Email Address',
+                       validators=[DataRequired(), length(max=200)],
+                       render_kw={"placeholder": "Email", "class": "input"})
+    message = TextAreaField('Message',
+                            validators=[DataRequired(), length(max=5000)],
+                            render_kw={"placeholder": "Enter your message here...",
+                                       "class": "input"})
+    submit = SubmitField('Send', render_kw={"class": "button is-link"})
+
+
 def about():
     with open('data/skills.json') as skills_f:
         skills = loads(skills_f.read())
