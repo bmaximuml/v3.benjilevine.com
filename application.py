e02814ce66ee888cf0190fbb22c0ef669c729a02
Benji Levine <benji@benjilevine.com>
2019-10-26 18:49:21 +0200

    Add more content (#16)
    * Correct indentation
    
    * Add contact form HTML
    
    * Add contact section
    
    * Update bolded text in about
    
    * Add skills icons
    
    * Use json to store skill data
    
    * Only show skill if name is set
    
    * Only show skill logo if skill.img is set
    
    * Assume skills logos stored in assets/icons
    
    * Add more skills

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 from flask import Flask, render_template
+from json import loads
 application = Flask(__name__)
 
 
 @application.route('/')
 def about():
-    return render_template('index.html', year=datetime.now().year)
+    with open('data/skills.json') as skills_f:
+        skills = loads(skills_f.read())
+
+    return render_template('index.html', year=datetime.now().year, skills=skills['skills'])
 
 
 if __name__ == '__main__':
