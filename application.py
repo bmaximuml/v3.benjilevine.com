81fcb3edf95a02bf5b44bc26de0ed944fb3c26e8
Benji Levine <benji@benjilevine.com>
2019-10-26 17:27:16 +0200

    Refactor (#14)
    * Add appropriate spacing
    
    * Reorder imports

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
 from datetime import datetime
+from flask import Flask, render_template
 application = Flask(__name__)
+
 
 @application.route('/')
 def about():
     return render_template('index.html', year=datetime.now().year)
+
 
 if __name__ == '__main__':
     application.debug = True
