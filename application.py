3d181a33261ff97c4dd74a874b2637afb5dbebcf
Benji Levine <benji@benjilevine.com>
2019-10-26 14:28:02 +0200

    Use json to store skill data

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 from datetime import datetime
+from json import loads
 application = Flask(__name__)
 
 @application.route('/')
 def about():
-    return render_template('index.html', year=datetime.now().year)
+    with open('data/skills.json') as skills_f:
+        skills = loads(skills_f.read())
+
+    return render_template('index.html', year=datetime.now().year, skills=skills['skills'])
 
 if __name__ == '__main__':
     application.debug = True
