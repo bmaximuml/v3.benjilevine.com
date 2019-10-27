8bbf929937d4eb5a2d9c9252bd723758be15990a
Benji Levine <benji@benjilevine.com>
2019-10-27 19:03:20 +0100

    Version bump to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='benjilevine.com',
-    version='0.2',
+    version='1.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
