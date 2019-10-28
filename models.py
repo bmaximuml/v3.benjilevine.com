874572bb8b8dee54c3d48ad7766ebe2b89b0ec1f
Benji Levine <benji@benjilevine.com>
2019-10-28 16:35:08 +0100

    Mobile display improvements (#47)
    * Hide top portrait on mobile
    
    * Improve project layout on mobile
    
    * Show skills as tags when on mobile
    
    * Open skill links in new tab
    
    * Link Skills with tags
    Using a foreign key from Skill to Tag, on name
    
    * Add python comparators to Tag
    
    * Display skill tags instead of cards on mobile
    
    * Hide Madrid photo on mobile
    
    * Slide menu in and out
    
    * Shrink contact button text on mobile
    
    * Improve HTML clarity for CV button
    
    * Improve project date display on mobile
    
    * Allow text folding in buttons
    
    * Display photo on mobile
    Beside About title
    
    * Display Madrid photo on mobile
    In CV section, beneath button

--- models.py
+++ models.py
@@ -11,9 +11,11 @@ class About(db.Model):
 
 
 class Skill(db.Model):
-    name = db.Column(db.String(200), primary_key=True, nullable=False)
+    name = db.Column(db.String(200), db.ForeignKey('tag.name'), primary_key=True, nullable=False)
     url = db.Column(db.String(500), nullable=True)
     img = db.Column(db.String(200), nullable=True)
+
+    tag = db.relationship("Tag", foreign_keys=name)
 
     def __repr__(self):
         return f'<Skill {str(self.name)}>'
@@ -52,3 +54,33 @@ class Project(db.Model):
 class Tag(db.Model):
     name = db.Column(db.String(200), primary_key=True, nullable=False)
     colour = db.Column(db.String(20), nullable=False)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.colour == other.colour
+
+    def __ne__(self, other):
+        if other is None:
+            return True
+        return self.colour != other.colour
+
+    def __lt__(self, other):
+        if other is None:
+            return False
+        return self.colour < other.colour
+
+    def __gt__(self, other):
+        if other is None:
+            return True
+        return self.colour > other.colour
+
+    def __le__(self, other):
+        if other is None:
+            return False
+        return self.colour <= other.colour
+
+    def __ge__(self, other):
+        if other is None:
+            return True
+        return self.colour >= other.colour
